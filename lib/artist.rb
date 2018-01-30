4bc192279d25d73733adfab20be0dcd8a93bbd73
Stephen Thurman <thurman024@gmail.com>
2018-01-30 15:45:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,14 @@ class Artist
     @songs = []
   end
 
-  def add_song
-    
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.add_song(song)
+  end
 
 end
